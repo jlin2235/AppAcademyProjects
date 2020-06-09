4be1e8e82a1edb3dee2928d285b2cda160397502
Jlin2235 <Jlin2235@gmail.com>
2020-06-09 16:33:27 -0400

    SQL Exercise

---
+++ W5D2/skeleton 3/lib/03a_nobel_bonus.rb
@@ -0,0 +1,33 @@
+# == Schema Information
+#
+# Table name: nobels
+#
+#  yr          :integer
+#  subject     :string
+#  winner      :string
+
+require_relative './sqlzoo.rb'
+
+def physics_no_chemistry
+  # In which years was the Physics prize awarded, but no Chemistry prize?
+  execute(<<-SQL)
+  SELECT 
+    DISTINCT yr
+  FROM 
+    nobels
+  WHERE
+    subject = 'Physics'
+    
+    EXCEPT
+    
+      SELECT
+        DISTINCT yr
+      FROM 
+        nobels
+      WHERE
+        subject = 'Chemistry'
+
+    
+
+  SQL
+end
